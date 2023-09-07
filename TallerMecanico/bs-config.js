e8be1e9fe0f60806589eba05dbde17c8c0a95521
FacundoGribaudo <gribaudofacundo13@gmail.com>
2023-09-07 18:30:54 -0300

    ABM Marca

---
+++ TallerMecanico/bs-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  server: {
+    baseDir: "C:/Users/facun/OneDrive/Documentos/GitHub/TallerMecanicoPA2023/TallerMecanico" // Ruta de la carpeta raíz de tu proyecto
+  },
+  files: ["C:/Users/facun/OneDrive/Documentos/GitHub/TallerMecanicoPA2023/TallerMecanico/src/main/resources/templates/*.html", "**/*.css", "**/*.js"] // Archivos a observar para cambios
+};
