e442eb50dae60ea4df95d0b7d16be60c14a86944
Tomi Gimenez <105307974+TomiGimenez@users.noreply.github.com>
2023-10-09 19:16:10 -0300

    Relacion servicios-ordenes

--- TallerMecanico/src/main/java/com/Proyecto/TallerMecanico/domain/OrdenTrabajo.java
+++ TallerMecanico/src/main/java/com/Proyecto/TallerMecanico/domain/OrdenTrabajo.java
@@ -23,7 +23,7 @@ public class OrdenTrabajo {
     @JoinColumn(name = "id_vehiculo")
     private Vehiculo vehiculoPertenece;
     
-    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
     @JoinColumn(name = "id_servicio")
     private ServiciosTaller servicioRealizar;  
 
--- TallerMecanico/src/main/java/com/Proyecto/TallerMecanico/domain/ServiciosTaller.java
+++ TallerMecanico/src/main/java/com/Proyecto/TallerMecanico/domain/ServiciosTaller.java
@@ -1,5 +1,8 @@
 package com.Proyecto.TallerMecanico.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -20,14 +23,18 @@ public class ServiciosTaller {
     @Column (name = "descripcion")
     private String descripcion;
 
+    @OneToMany (mappedBy = "servicioRealizar")
+    private List<OrdenTrabajo> ordenesTrabajo;
+
     public ServiciosTaller(){
 
     }
 
-    public ServiciosTaller(String nombre, String descripcion){
+    public ServiciosTaller(String nombre, String descripcion, List<OrdenTrabajo> ordenesTrabajo){
         super();
         this.nombre = nombre;
-        this.descripcion = descripcion; 
+        this.descripcion = descripcion;
+        this.ordenesTrabajo = new ArrayList<>();
     }
 
 }
