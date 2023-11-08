dee7bfd6d936aef0e9c218af338309b2a09790df
TomiGimenez <105307974+TomiGimenez@users.noreply.github.com>
2023-11-08 00:01:08 -0300

    Obtencion datos detalleOrden

--- TallerMecanico/src/main/resources/static/js/detalleOrden.js
+++ TallerMecanico/src/main/resources/static/js/detalleOrden.js
@@ -24,26 +24,39 @@ function limitarMinutosInput(input) {
     }
 }
 
+window.addEventListener('DOMContentLoaded', (event) => {
+    // Obten la última columna
+    const lastColumn = document.querySelector('.fixed-column');
+    
+    // Calcula la anchura de la última columna
+    const lastColumnWidth = lastColumn.offsetWidth;
+    
+    // Aplica la anchura a la última columna
+    lastColumn.style.width = lastColumnWidth + 'px';
+});
+
 
 // --- Calcular precio por horas y minutos ---
 // Obtener referencias a los elementos del DOM por su atributo "name"
 var precioPorHoraInputs = document.getElementsByName("precioHora");
 var horasTrabajadasInputs = document.getElementsByName("horas");
 var minutosTrabajadosInputs = document.getElementsByName("minutos");
-var subtotalInputs = document.getElementsByName("precio");
+var precioServicioInputs = document.getElementsByName("precio");
 var descuentoInputs = document.getElementsByName("%descuento");
 var montodescuentoInputs = document.getElementsByName("montodescuento");
 var impuestoInputs = document.getElementsByName("%impuesto");
 var montoImpuestosInputs = document.getElementsByName("montoImpuestos");
 var totalInputs = document.getElementsByName("totalServicio");
+var totalDescuentosInputs = document.getElementsByName("totalDescuentos");
+var totalImpuestosInputs = document.getElementsByName("totalImpuestos");
+var subtotalInputs = document.getElementsByName("subtotal");
+var totalHorasInputs = document.getElementsByName("horasTrabajadas");
+var totalMinutosInputs = document.getElementsByName("minutosTrabajados");
 
 // Agregar event listeners para detectar cambios en los campos de entrada
 for (var i = 0; i < precioPorHoraInputs.length; i++) {
-    precioPorHoraInputs[i].addEventListener("input", calcularTotal);
     horasTrabajadasInputs[i].addEventListener("input", calcularTotal);
     minutosTrabajadosInputs[i].addEventListener("input", calcularTotal);
-    descuentoInputs[i].addEventListener("input", calcularTotal);
-    impuestoInputs[i].addEventListener("input", calcularTotal);
 }
 
 // Función para calcular el total en tiempo real
@@ -55,30 +68,60 @@ function calcularTotal() {
         var descuento = parseFloat(descuentoInputs[i].value) || 0;
         var impuesto = parseFloat(impuestoInputs[i].value) || 0;
 
-        var subtotal = precioHora * (horas + minutos / 60);
-        var descuentoAmount = subtotal * (descuento / 100);
-        var impuestoAmount = subtotal * (impuesto / 100);
+        var totalServicio = precioHora * (horas + minutos / 60);
+        var descuentoServicio = totalServicio * (descuento / 100);
+        var impuestoServicio = totalServicio * (impuesto / 100);
 
-        var total = subtotal - descuentoAmount + impuestoAmount;
-        subtotalInputs[i].value = subtotal.toFixed(2);
+        var total = totalServicio - descuentoServicio + impuestoServicio;
+        precioServicioInputs[i].value = totalServicio.toFixed(2);
         totalInputs[i].value = total.toFixed(2);
-        montodescuentoInputs[i].value = descuentoAmount.toFixed(2);
-        montoImpuestosInputs[i].value = impuestoAmount.toFixed(2);
+        montodescuentoInputs[i].value = descuentoServicio.toFixed(2);
+        montoImpuestosInputs[i].value = impuestoServicio.toFixed(2);
+    
+
     }
+    // Calcular el subtotal
+    var subtotal = 0;
+    for (var i = 0; i < totalInputs.length; i++) {
+        subtotal += parseFloat(totalInputs[i].value) || 0;
+    }
+    subtotalInputs[0].value = subtotal.toFixed(2);
+
+    // Calcular el total de descuentos
+    var totalDescuentos = 0;
+    for (var i = 0; i < montodescuentoInputs.length; i++) {
+        totalDescuentos += parseFloat(montodescuentoInputs[i].value) || 0;
+    }
+    totalDescuentosInputs[0].value = totalDescuentos.toFixed(2);
+
+    // Calcular el total de impuestos
+    var totalImpuestos = 0;
+    for (var i = 0; i < montoImpuestosInputs.length; i++) {
+        totalImpuestos += parseFloat(montoImpuestosInputs[i].value) || 0;
+    }
+    totalImpuestosInputs[0].value = totalImpuestos.toFixed(2);
+
+    // Calcular el total de horas y minutos trabajados
+    var totalHoras = 0;
+    var totalMinutos = 0;
+
+    for (var i = 0; i < horasTrabajadasInputs.length; i++) {
+        totalHoras += parseFloat(horasTrabajadasInputs[i].value) || 0;
+        totalMinutos += parseFloat(minutosTrabajadosInputs[i].value) || 0;
+    }
+
+    // Convertir los minutos a horas si es necesario
+    if (totalMinutos >= 60) {
+        var horasExtras = Math.floor(totalMinutos / 60);
+        totalHoras += horasExtras;
+        totalMinutos %= 60;
+    }
+
+    // Mostrar el total de horas y minutos trabajados
+    totalHorasInputs[0].value = totalHoras;
+    totalMinutosInputs[0].value = totalMinutos;
 }
 
 // Calcular los totales iniciales
 calcularTotal();
 
-
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    // Obten la última columna
-    const lastColumn = document.querySelector('.fixed-column');
-    
-    // Calcula la anchura de la última columna
-    const lastColumnWidth = lastColumn.offsetWidth;
-    
-    // Aplica la anchura a la última columna
-    lastColumn.style.width = lastColumnWidth + 'px';
-});
